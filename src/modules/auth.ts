148c6ecd742f9ee0cb17efabeeabce76e4b7e274
Andrew Solomon <asolomon@magmutual.com>
2024-04-15 22:00:50 -0400

    DM-15 – Add proper next config and fix build for vercel deployment

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,11 +1,11 @@
 import jwt from 'jsonwebtoken';
 
-export const createToken = (payload: object) => {
-  return jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: '1h',
-  });
-};
-
-export const protect = (req, res) => {
-    const bearer = req.headers.authorization
-})
+// export const createToken = (payload: object) => {
+//   return jwt.sign(payload, process.env.JWT_SECRET, {
+//     expiresIn: '1h',
+//   });
+// };
+//
+// export const protect = (req, res) => {
+//     const bearer = req.headers.authorization
+// })
