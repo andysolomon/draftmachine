1d54af06aaaaa0d0bded143e244d3e1def9ee72a
Andrew Solomon <asolomon@magmutual.com>
2024-04-30 20:54:50 -0400

    DM-23 – Print Athlete table to dom

--- src/app/[[...slug]]/client.tsx
+++ src/app/[[...slug]]/client.tsx
@@ -2,17 +2,16 @@
  
 import React from 'react'
 import dynamic from 'next/dynamic'
+// import { IAthlete } from '../../../draftmachine'
  
 // const App = dynamic(() => import('../../App'), { ssr: false })
  
-export function ClientOnly({ athletes }) {
+export function ClientOnly() {
     return(
         <div>
             <h1 className="text-3xl font-bold underline">Client Only</h1>
             <ul>
-                {athletes.map((athlete) => (
-                    <li key={athlete.id}>{athlete.name}</li>
-                ))}
+
             </ul>
         </div>
 
