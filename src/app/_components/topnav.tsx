ae5e62d8fff9c9c416f1679b7ae73e4ba2251909
Andrew Solomon <asolomon@magmutual.com>
2024-04-30 21:38:18 -0400

    DM-25 – Create Home/Landing Page
    - Update TopNav moving login button to right

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -4,20 +4,13 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-reac
 
 export function TopNav() {
     return (
-        <nav className="flex">
-            <ul>
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    <SignedOut>
-                        <SignInButton />
-                    </SignedOut>
-                    <SignedIn>
-                        <UserButton />
-                    </SignedIn>
-                </li>
-            </ul>
+        <nav className="flex justify-end">
+            <SignedOut>
+                <SignInButton />
+            </SignedOut>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
         </nav>
     )
 }
