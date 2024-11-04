299b610aa3dabc7cd15fbea72289b106d867b4a9
Andrew Solomon <asolomon@magmutual.com>
2024-11-03 20:26:42 -0500

    DM-26 – Create Athlete Page

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,12 @@
-import { type Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
 import { env } from "~/env";
 
-export default {
-  schema: "./src/server/db/schema.ts",
-  driver: "pg",
-  dbCredentials: {
-    connectionString: env.POSTGRES_URL,
-  },
-  tablesFilter: ["draftmanager_*"],
-  verbose: true,
-  strict: true,
-  out: "./drizzle",
-} satisfies Config;
+export default defineConfig({
+    schema: "./src/server/db/schema.ts",
+    out: "./drizzle",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: env.POSTGRES_URL,
+    },
+});
---
+++ src/app/create/athlete/page.tsx
@@ -0,0 +1,14 @@
+export default function CreateAthlete() {
+    return (
+        <main>
+            <h1>Create Athlete</h1>
+            <form>
+                <label>First Name</label>
+                <input type="text" />
+                <label>Last Name</label>
+                <input type="text" />
+                <button type="submit">Create</button>
+            </form>
+        </main>
+    )
+}
